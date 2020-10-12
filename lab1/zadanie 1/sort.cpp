d7f2e5ccafea793b56a06f3d51644426366857f6
Dawid Banach <dawid@friqtechnology.com>
2020-10-12 13:47:01 +0200

    Completed zadanie1 - sort.cpp

--- lab1/zadanie 1/sort.cpp
+++ lab1/zadanie 1/sort.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main() 
+int main(int argc, char *argv[])
 {
-    std::cout << "To jest algorytm sortujacy";
+    //rezerwacja tablicy na liczby
+    int* tablica_sort = (int*) malloc(sizeof(int));
+
+    // wypisanie argumentow
+    cout << "Liczba argumentow: " << (argc - 1) << endl;
+    cout << "Argumenty: "<< endl;
+    cout << "input: ";
+    for (int i = 1; i < argc; ++i) 
+    cout << argv[i] <<" ";
+
+    // dodanie i konwersja argumentow do tablicy sort
+    for (int i = 0; i < argc; i++)
+    tablica_sort[i] = atoi(argv[i+1]);
+
+    // sortowanie 
+    sort(tablica_sort,tablica_sort + (argc-1));
+
+    // wypisanie wyniku
+    cout << endl;
+    cout << "out: ";
+    for( int i = 0; i < (argc - 1); i++ )
+         cout << tablica_sort[i] << " ";
+
     return 0;
 }
